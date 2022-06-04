bbdd64f3698911d616537790dda04607b8530162
Blair Guardia <blairguardia@gmail.com>
2022-06-04 21:30:41 +0900

    Update modal header styling

--- src/utilities/components.js
+++ src/utilities/components.js
@@ -35,7 +35,7 @@ const components = {
 		let modal = toHTML('<div class="modal" tabindex="-1">' +
 								'<div class="modal-dialog">' +
 									'<div class="modal-content">' +
-										'<div class="modal-header">' +
+										'<div class="modal-header modal-header--todo-app">' +
 											`<h5 class="modal-title">${title}</h5>` +
 											`<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>` +
       									`</div>` +
